3faf64bd46eec9663d92c6f3f4cfa40e00271e96
Antoine Amiguet <amiguet.antoine@gmail.com>
2020-12-18 13:40:58 +0100

    Day 17 completed

--- src/day17.js
+++ src/day17.js
@@ -5,10 +5,8 @@ const fs = require('fs');
 const dataPath = 'data/2020_day_17_input.txt';
 
 class Cube {
-  constructor(x, y, z, state) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
+  constructor(coords, state) {
+    this.coords = coords;
     this.state = state;
     this.neighbors = [];
   }
@@ -19,11 +17,9 @@ class Cube {
 
   canBeNeighbor(cube) {
     return (
-      (this.x !== cube.x || this.y !== cube.y || this.z !== cube.z) &&
-      Math.abs(this.x - cube.x) <= 1 &&
-      Math.abs(this.y - cube.y) <= 1 &&
-      Math.abs(this.z - cube.z) <= 1 &&
-      (!this.neighbors.includes(cube))
+      this.coords.filter((c, index) => c !== cube.coords[index]).length > 0 &&
+      this.coords.every((c, index) => Math.abs(c - cube.coords[index]) <= 1) &&
+      !this.neighbors.includes(cube)
     );
   }
 
@@ -37,12 +33,20 @@ class Cube {
   }
 }
 
-function initPocket(xLength, yLength, zLength, cycles) {
+function extractLines() {
+  return fs
+  .readFileSync(dataPath)
+  .toString()
+  .split(/\n/)
+  .filter(l => l.trim() !== '');
+}
+
+function initPocket3D(xLength, yLength, zLength, cycles) {
   let cubes = [];
   for (let x = -cycles; x <= xLength + cycles; x++) {
     for (let y = -cycles; y <= yLength + cycles; y++) {
       for (let z = -cycles; z <= zLength + cycles; z++) {
-        cubes.push(new Cube(x, y, z, false));
+        cubes.push(new Cube([x, y, z], false));
       }
     }
   }
@@ -52,35 +56,65 @@ function initPocket(xLength, yLength, zLength, cycles) {
   return cubes;
 }
 
-function extractData(cycles) {
-  const lines = fs
-    .readFileSync(dataPath)
-    .toString()
-    .split(/\n/)
-    .filter(l => l.trim() !== '');
-  const cubes = initPocket(lines[0].length, lines.length, 0, cycles);
+function extractData3D(cycles) {
+  const lines = extractLines();
+  const cubes = initPocket3D(lines[0].length, lines.length, 0, cycles);
   cubes.forEach((c) => {
-    if (c.z === 0 && typeof lines[c.y] !== 'undefined' && typeof lines[c.y][c.x] !== 'undefined') {
-      c.state = lines[c.y][c.x] === '#';
+    if (c.coords[2] === 0 && typeof lines[c.coords[1]] !== 'undefined' && typeof lines[c.coords[1]][c.coords[0]] !== 'undefined') {
+      c.state = lines[c.coords[1]][c.coords[0]] === '#';
     }
   });
   return cubes;
 }
 
+function initPocket4D(xLength, yLength, zLength, wLength, cycles) {
+  // this is obviously an awful way of doing it, let's hope we don't get more dimensions :)
+  let cubes = [];
+  for (let x = -cycles; x <= xLength + cycles; x++) {
+    for (let y = -cycles; y <= yLength + cycles; y++) {
+      for (let z = -cycles; z <= zLength + cycles; z++) {
+        for (let w = -cycles; w <= wLength + cycles; w++) {
+          cubes.push(new Cube([x, y, z, w], false));
+        }
+      }
+    }
+  }
+  cubes.forEach((c, index, cs) => {
+    cs.forEach(n => c.addNeighbor(n));
+  });
+  return cubes;
+}
+
+function extractData4D(cycles) {
+  const lines = extractLines();
+  const cubes = initPocket4D(lines[0].length, lines.length, 0, 0, cycles);
+  cubes.forEach((c) => {
+    if (c.coords[3] === 0 && c.coords[2] === 0 && typeof lines[c.coords[1]] !== 'undefined' && typeof lines[c.coords[1]][c.coords[0]] !== 'undefined') {
+      c.state = lines[c.coords[1]][c.coords[0]] === '#';
+    }
+  });
+  return cubes;
+}
+
+function solve(extractData) {
+  const cycles = 6;
+  const cubes = extractData(cycles);
+  for (let i = 0; i < cycles; i++) {
+    const nextStates = cubes.map(c => c.nextState());
+    nextStates.forEach((v, index) => {
+      cubes[index].state = v;
+    });
+  }
+  const activeCubes = cubes.filter(c => c.state).length;
+  console.log(`There are ${activeCubes} active cubes after ${cycles} cycles`);
+}
+
 module.exports = {
   solve1: () => {
-    const cycles = 6;
-    const cubes = extractData(cycles);
-    for (let i = 0; i < cycles; i++) {
-      const nextStates = cubes.map(c => c.nextState());
-      nextStates.forEach((v, index) => {
-        cubes[index].state = v;
-      });
-    }
-    const activeCubes = cubes.filter(c => c.state).length;
-    console.log(`There are ${activeCubes} active cubes after ${cycles} cycles`);
+    solve(extractData3D);
   },
 
   solve2: () => {
+    solve(extractData4D);
   }
 }
