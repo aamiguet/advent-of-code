1935ce7b574ec3ebdf03fc4cfbf4ffccf8d8149f
Antoine Amiguet <amiguet.antoine@gmail.com>
2020-12-29 17:08:28 +0100

    Day 23 part II completed

--- src/day23.js
+++ src/day23.js
@@ -1,58 +1,87 @@
-const PickUpSize = 3;
-const Highest = 9;
 
-const TestState = {
-  cups: [3,8,9,1,2,5,4,6,7],
-  index: 0,
-  move: 10
-};
-const InputState = {
-  cups: [1, 2, 3, 4, 8, 7, 5, 9, 6],
-  index: 0,
-  move: 100
-};
 
-function destination(current, pickedUp) {
+const TestCups = [3, 8, 9, 1, 2, 5, 4, 6, 7];
+const InputCups = [1, 2, 3, 4, 8, 7, 5, 9, 6];
+
+function destination(current, pickedUp, highest) {
   let d = current;
   do {
-    d = d === 1 ? Highest : d - 1;
+    d = d === 1 ? highest : d - 1;
   } while (pickedUp.includes(d));
   return d;
 }
 
-function move(state) {
-  const current = state.cups[state.index];
-  let relativeCups = state.cups.slice(state.index + 1).concat(state.cups.slice(0, state.index));
-  const pickedUp = relativeCups.splice(0, PickUpSize);
-  const dest = destination(current, pickedUp);
-  const destIndex = relativeCups.indexOf(dest);
-  const newRelativeCups = relativeCups.slice(0, destIndex + 1).concat(pickedUp).concat(relativeCups.slice(destIndex + 1));
-  const leftCups = state.index > 0 ? newRelativeCups.slice(-state.index) : [];
-  const rightCups = state.index < newRelativeCups.length ? newRelativeCups.slice(0, newRelativeCups.length - state.index) : [];
-  const newCups = leftCups.concat([current]).concat(rightCups);
-  const newIndex = state.index === newCups.length - 1 ? 0 : state.index + 1;
-  return {
-    cups: newCups,
-    index: newIndex,
-    move: state.move
-  };
+function print(cupMap) {
+  let current = cupMap.get(1);
+  let val = [];
+  while (current !== 1) {
+    val.push(current);
+    current = cupMap.get(current);
+  }
+  return val.join('');
 }
 
-function print(state) {
-  const index = state.cups.indexOf(1);
-  const arrangedCups = state.cups.slice(index + 1).concat(state.cups.slice(0, index));
-  return arrangedCups.join('');
+function extend(cups, highest) {
+  let c = cups.slice();
+  const start = Math.max(...cups) + 1;
+  for (let i = start; i <= highest; i++) {
+    c.push(i);
+  }
+  return c;
+}
+
+function asMap(cups) {
+  let m = new Map();
+  for (let i = 0; i < cups.length; i++) {
+    m.set(cups[i], cups[(i + 1) % cups.length]);
+  }
+  return m;
+}
+
+/**
+ * Updates the cup map and returns the next key
+ *
+ * @param {*} cupMap
+ * @param {*} key
+ * @param {*} highest
+ */
+function updateMap(cupMap, key, highest) {
+  const pickUpSize = 3;
+  let pickedUp = [cupMap.get(key)];
+  for (let i = 1; i < pickUpSize; i++) {
+    pickedUp.push(cupMap.get(pickedUp[i-1]));
+  }
+  const dest = destination(key, pickedUp, highest);
+  // key -> value following the last removed value
+  cupMap.set(key, cupMap.get(pickedUp[pickedUp.length - 1]));
+  // last removed value -> value following the destination
+  cupMap.set(pickedUp[pickedUp.length - 1], cupMap.get(dest));
+  // destination -> first removed value
+  cupMap.set(dest, pickedUp[0]);
+  return cupMap.get(key);
 }
 
 module.exports = {
   solve1: () => {
-    let state = InputState;
-    for (let i = 0; i < state.move; i++) {
-      state = move(state);
+    const cups = InputCups;
+    const highest = Math.max(...cups);
+    let cupMap = asMap(cups);
+    let key = cups[0]
+    for (let i = 0; i < 100; i++) {
+      key = updateMap(cupMap, key, highest);
     }
-    console.log(print(state));
+    console.log(print(cupMap));
   },
 
   solve2: () => {
+    const heighest = 1000000;
+    const cups = extend(InputCups, heighest);
+    let cupMap = asMap(cups);
+    let key = cups[0];
+    for (let i = 0; i < 10000000; i++) {
+      key = updateMap(cupMap, key, heighest);
+    }
+    const next = cupMap.get(1);
+    console.log(`The following values are ${next} and ${cupMap.get(next)}. Their product is ${next * cupMap.get(next)}`);
   }
 }
